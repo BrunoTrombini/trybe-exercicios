f6304df7fa8242cc256d9a3aae319084feb5fe4f
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-20 19:13:56 -0300

    Iniciando a parte 2 do exercício 4

---
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 01 - JavaScript - Primeiros passos/scripts3.js
@@ -0,0 +1,15 @@
+const a = 5;
+const b = 10;
+
+let adição = a + b;
+let subtração = a - b;
+let multiplicação = a * b;
+let divisão = a / b;
+let módulo = a % b;
+
+console.log(adição);
+console.log(subtração);
+console.log(multiplicação);
+console.log(divisão);
+console.log(módulo);
+
