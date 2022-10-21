8f01ae2f721558d48f1cfd6661239a3c17fa2363
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-21 18:22:52 -0300

    Finalizando parte 3

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/scripts1.js
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/scripts1.js
@@ -4,9 +4,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log(numbers[i]);
 // }
 
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     soma += numbers[i];
+// }
+// console.log(soma);
+
 let soma = 0;
+let media = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
     soma += numbers[i];
+    media = soma / numbers.length
 }
-console.log(soma);
+console.log(media);
