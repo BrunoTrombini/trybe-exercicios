e3a2fcc677216dedf26bf5f9fdeafe95b39a4441
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-21 18:32:08 -0300

    Finalizando parte 5

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/scripts1.js
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/scripts1.js
@@ -4,32 +4,40 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //     console.log(numbers[i]);
 // }
 
-// let soma = 0;
 
+// let soma = 0;
 // for (let i = 0; i < numbers.length; i += 1) {
 //     soma += numbers[i];
 // }
 // console.log(soma);
 
+
 // let soma = 0;
 // let media = 0;
-
 // for (let i = 0; i < numbers.length; i += 1) {
 //     soma += numbers[i];
 //     media = soma / numbers.length
 // }
 // console.log(media);
 
-let soma = 0;
-let media = 0;
 
+// let soma = 0;
+// let media = 0;
+// for (let i = 0; i < numbers.length; i += 1) {
+//     soma += numbers[i];
+//     media = soma / numbers.length
+// }
+// if (media > 20) {
+//     console.log('valor maior que 20')
+// }
+// else {
+//     console.log('valor menor que 20')
+// }
+
+let maiorNumero = numbers[0]
 for (let i = 0; i < numbers.length; i += 1) {
-    soma += numbers[i];
-    media = soma / numbers.length
+    if(numbers[i] > maiorNumero) {
+        maiorNumero = numbers[i]
+    }
 }
-if (media > 20) {
-    console.log('valor maior que 20')
-}
-else {
-    console.log('valor menor que 20')
-}
+console.log(maiorNumero);
