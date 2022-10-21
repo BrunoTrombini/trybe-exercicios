cf1eb5f8a8749dc3e8d70ab9f2834499fafaf179
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-21 18:25:18 -0300

    Finalizando parte 4

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/scripts1.js
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 02 - JavaScript - Array e loop For/scripts1.js
@@ -11,6 +11,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 // console.log(soma);
 
+// let soma = 0;
+// let media = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//     soma += numbers[i];
+//     media = soma / numbers.length
+// }
+// console.log(media);
+
 let soma = 0;
 let media = 0;
 
@@ -18,4 +27,9 @@ for (let i = 0; i < numbers.length; i += 1) {
     soma += numbers[i];
     media = soma / numbers.length
 }
-console.log(media);
+if (media > 20) {
+    console.log('valor maior que 20')
+}
+else {
+    console.log('valor menor que 20')
+}
