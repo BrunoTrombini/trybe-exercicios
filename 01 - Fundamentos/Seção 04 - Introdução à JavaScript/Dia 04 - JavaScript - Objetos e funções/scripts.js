8ce05722f0ee030f4cb11f59a575fcb3ae89cc60
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-24 19:48:32 -0300

    movendo os arquivos para pasta certa

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 04 - JavaScript - Objetos e funções/scripts.js
+++
@@ -1,5 +0,0 @@
-// O fatorial é representado pelo sinal !
-// 4! = 4 x 3 x 2 x 1 = 24
-
-let fatorial = 4!;
-console.log(fatorial);
--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 04 - JavaScript - Objetos e funções/scriptsBonus.js
+++
@@ -1,6 +0,0 @@
-let primo = [];
-for (let i = 2; i <= 50; i += 1) {
-    if(i % i == 0 && i % 2 != 0 && i % 3 != 0 && i % 5 != 0);
-    primo += primo[i];
-    console.log(primo);
-}
