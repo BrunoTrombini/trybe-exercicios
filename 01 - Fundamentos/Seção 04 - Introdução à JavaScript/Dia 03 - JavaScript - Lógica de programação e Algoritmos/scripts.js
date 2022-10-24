a9991e8e0eb15ba9557dad6a8bc5369e6b3d8371
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-24 20:03:44 -0300

    Finalizando o exercício de fatorial

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scripts.js
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scripts.js
@@ -1,5 +1,8 @@
 // O fatorial é representado pelo sinal !
 // 4! = 4 x 3 x 2 x 1 = 24
 
-let fatorial = 4!;
+let fatorial = 1;
+for (let i = 1; i <= 10; i += 1) {
+    fatorial*= i;
+}
 console.log(fatorial);
