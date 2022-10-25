6ddc1b3e0a5e183e995d28c4e7b3ee46a84db3d5
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-24 21:14:34 -0300

    Finalizando exercício de maior e menor palavra

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scripts.js
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scripts.js
@@ -1,14 +1,32 @@
 // let fatorial = 1;
 // for (let i = 1; i <= 10; i += 1) {
-//     fatorial*= i;
+//     fatorial *= i;
 // }
 // console.log(fatorial);
 
 
-let word = 'trybe';
-let anagrama = [];
+// let word = 'trybe';
+// let anagrama = [];
 
-for (let i = 0; i < word.length; i += 1) {
-    anagrama.unshift(word[i]);        
+// for (let i = 0; i < word.length; i += 1) {
+//     anagrama.unshift(word[i]);
+// }
+// console.log(anagrama);
+
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for (let i = 0; i < array.length; i += 1) {
+    if (maiorPalavra.length < array[i].length) {
+        maiorPalavra = array[i];
+        console.log(`Maior palavra é ${maiorPalavra} !`);
+    }
 }
-console.log(anagrama);
+for (let i = 0; i < array.length; i += 1) {
+    if (menorPalavra.length > array[i].length) {
+        menorPalavra = array[i];
+        console.log(`Menor palavra é ${menorPalavra} !`);
+    }
+}
