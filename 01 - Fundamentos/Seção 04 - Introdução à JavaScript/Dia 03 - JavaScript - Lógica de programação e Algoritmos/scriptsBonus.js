80b4e9e73329c37a2a1ecbdffa6378305782f221
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-24 21:52:01 -0300

    Finalizando a parte 1 do exercício bônus

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scriptsBonus.js
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scriptsBonus.js
@@ -1,6 +1,10 @@
-let primo = 0;
-for (let i = 2; i <= 50; i += 1) {
-    if(i % i == 0 && i % 2 != 0 && i % 3 != 0 && i % 5 != 0);
-    primo += i;
-    console.log(primo);
+let n = 5;
+let simbolo = '*';
+let figura = '';
+
+for (let i = 0; i < n; i += 1) {
+    figura += simbolo;
+}
+for (let i = 0; i < n; i += 1) {
+    console.log(figura);
 }
