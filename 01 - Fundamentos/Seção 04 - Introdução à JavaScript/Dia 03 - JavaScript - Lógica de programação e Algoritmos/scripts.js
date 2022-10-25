ace91d8a860c21599ae89c79af5fe03b9f3ef332
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-24 21:36:42 -0300

    Finalizando exercício de números primos

--- 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scripts.js
+++ 01 - Fundamentos/Seção 04 - Introdução à JavaScript/Dia 03 - JavaScript - Lógica de programação e Algoritmos/scripts.js
@@ -14,19 +14,35 @@
 // console.log(anagrama);
 
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let maiorPalavra = array[0];
-let menorPalavra = array[0];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = array[0];
+// let menorPalavra = array[0];
 
-for (let i = 0; i < array.length; i += 1) {
-    if (maiorPalavra.length < array[i].length) {
-        maiorPalavra = array[i];
-        console.log(`Maior palavra é ${maiorPalavra} !`);
+// for (let i = 0; i < array.length; i += 1) {
+//     if (maiorPalavra.length < array[i].length) {
+//         maiorPalavra = array[i];
+//         console.log(`Maior palavra é ${maiorPalavra} !`);
+//     }
+// }
+// for (let i = 0; i < array.length; i += 1) {
+//     if (menorPalavra.length > array[i].length) {
+//         menorPalavra = array[i];
+//         console.log(`Menor palavra é ${menorPalavra} !`);
+//     }
+// }
+
+
+let maiorPrimo = 0;
+
+for (let numero = 2; numero <= 50; numero += 1) {
+    primo = true;
+    for (let divisor = 2; divisor < numero; divisor += 1) {
+        if (numero % divisor === 0) {
+            primo = false;
+        }
     }
+    if (primo) {
+        maiorPrimo = numero;
 }
-for (let i = 0; i < array.length; i += 1) {
-    if (menorPalavra.length > array[i].length) {
-        menorPalavra = array[i];
-        console.log(`Menor palavra é ${menorPalavra} !`);
-    }
 }
+console.log(maiorPrimo);
