a6abe3958b0159d09873c561d8f7a620a57ff782
Bruno Trombini <brunotrombini@yahoo.com.br>
2022-10-28 15:29:55 -0300

    Terminando exercício para fixar, parte 1

---
+++ 01 - Fundamentos/Seção 05 - JavaScript: DOM, Eventos e Web Storage/Dia 01 - JavaScript - DOM e Seletores/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('page-title').innerText = 'Eu sou a lenda!';
+
+document.getElementById('second-paragraph').innerText = 'Melhores filmes do Will Smith.';
+
+document.getElementById('subtitle').innerText = `Eu Robô, 7 vidas, Golpe duplo, Hancock.`
